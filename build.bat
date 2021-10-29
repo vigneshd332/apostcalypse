e248034786be22173839c1929471aa9635c6b3e0
Vignesh Duraisamy <vigneshd332@gmail.com>
2021-10-30 02:55:15 +0530

    Add Batch and CMake Files

---
+++ build.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+ECHO You are about to commence the build, after the build, an executable 'apost.exe' will be created.
+ECHO Make sure you have cmake and make installed.
+ECHO.
+PAUSE
+
+ECHO.
+ECHO Generating Makefile...
+cmake . -G "MinGW Makefiles"
+
+ECHO.
+ECHO Building...
+make
+
+ECHO.
+ECHO Build Done...
+
+ECHO.
+ECHO Cleaning Up...
+del cmake_install.cmake
+del CMakeCache.txt
+del Makefile
+del CMakeFiles
+ECHO Done.
