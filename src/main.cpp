8f3f227c19696070c0f63724f25a50ab624637ea
Vignesh Duraisamy <vigneshd332@gmail.com>
2021-11-01 18:41:13 +0530

    Structured Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
+#include <string>
 #include <Poco/Net/HTTPClientSession.h>
 #include <Poco/Net/HTTPRequest.h>
 #include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPMessage.h>
 #include <Poco/Net/HTMLForm.h>
 #include <Poco/StreamCopier.h>
 
 using namespace std;
 using namespace Poco::Net;
+string url = "www.example.com";
 
-
-int main(){
-    HTTPClientSession s("www.example.com");
-    cout<<"Connected"<<endl;
-    //s.setProxy("localhost", srv.port());
-    HTTPRequest request(HTTPRequest::HTTP_GET, "/");
-    //HTMLForm form;
-    //form.add("entry1", "value1");
-    //form.prepareSubmit(request);
-    cout<<"Request Prepared"<<endl;
-    s.sendRequest(request);
-
-    cout<<"Request Sent, awaiting Response..."<<endl;
+void getReq(HTTPClientSession& s){
+    HTTPRequest request(HTTPRequest::HTTP_GET, "/", HTTPMessage::HTTP_1_1);
+    cout<<"GET Request Prepared"<<endl;
     HTTPResponse response;
+    s.sendRequest(request);
+    cout<<"Request Sent, awaiting Response..."<<endl;
     istream& rs = s.receiveResponse(response);
     cout<<"Response Received"<<endl;
-    Poco::StreamCopier::copyStream(rs, cout);
+    cout << response.getStatus() << endl;
+    //Poco::StreamCopier::copyStream(rs, cout);
+}
+
+void postReq(HTTPClientSession& s){
+    HTTPRequest request(HTTPRequest::HTTP_POST, "/", HTTPMessage::HTTP_1_1);
+    HTMLForm form;
+    form.set("name", "value");
+    form.set("name2", "value2");
+    form.prepareSubmit(request);
+    cout<<"Request Prepared"<<endl;
+    HTTPResponse response;
+    s.sendRequest(request);
+    cout<<"Request Sent, awaiting Response..."<<endl;
+    istream& rs = s.receiveResponse(response);
+    cout<<"Response Received"<<endl;
+    cout << response.getStatus() << endl;
+}
+
+int main(){
+    HTTPClientSession s(url);
+    cout<<"Connected"<<endl;
+    //s.setProxy("localhost", srv.port());
+    getReq(s);
+    postReq(s);
     return 0;
 }
